981a27e5d529bf1fb006cada22ab8a9e35a1d20e
alfahadFF <fhd574095@gmail.com>
2026-08-31 20:59:46 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,3 @@
 self.addEventListener('fetch', (event) => {
-  // تمرير الطلبات بشكل طبيعي عبر الشبكة لضمان عمل البطاقة بسلاسة
   event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
